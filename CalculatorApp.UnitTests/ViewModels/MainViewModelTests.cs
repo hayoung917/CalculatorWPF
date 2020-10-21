545469d39154d5914ff329645ad61d3ab96e68ea
하영 이 <hayoung.lee917@gmail.com>
2020-10-21 10:24:06 +0900

    불필요한 유징 제거하였습니다.

--- CalculatorApp.UnitTests/ViewModels/MainViewModelTests.cs
+++ CalculatorApp.UnitTests/ViewModels/MainViewModelTests.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using CalculatorApp.Models;
 using CalculatorApp.ViewModels;
 using FluentAssertions;
-using Saige.MVVM;
 using Xunit;
 
 namespace CalculatorApp.UnitTests.ViewModels
